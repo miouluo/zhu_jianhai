0bfe01808b52596367577f605e02328018542e77
zuxi_zeng <2171232322@qq.com>
2023-10-24 21:44:12 +0800

    bmi

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 a=float(input("请输入你的身高（单位m）："))
 b=float(input("请输入你的体重（kg）："))
 c= b/(a)**2
-print("你的bmi值是"+str(c))
+if float(c)>23:
+    print("你的bmi值是"+str(c)+"偏胖")
+else:
+    print("你的bmi值是" + str(c)+"正常")
