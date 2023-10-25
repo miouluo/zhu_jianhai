933a6d267c65131e9fdf8bfb48e2b18ae5a068e2
Lenovo <2409773670@qq.com>
2023-10-26 00:44:35 +0800

    study

--- 搜狗网页爬虫.py
+++ 搜狗网页爬虫.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     #  获取响应数据，text返回的是字符串形式的响应数据
     page_text = response.text
     print(page_text)
-    #  持久化存储
+    # 持久化存储
     with open('./搜狗网页爬虫.html', 'w', encoding='utf-8') as fp:
         fp.write(page_text)
     print('爬取数据结束！！！')
