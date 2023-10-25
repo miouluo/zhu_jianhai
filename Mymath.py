1f9e0ed61fe6b765f0a5225ee21963f24b343375
jwl77 <3266461662@qq.com>
2023-10-25 18:04:28 +0000

    update Mymath.py.
    Signed-off-by: jwl77 <3266461662@qq.com>

--- Mymath.py
+++ Mymath.py
@@ -1,12 +1,13 @@
-def zhishu(x):
-    x = eval(input('请输入数据:'))
-    if x <= 1:
+def zhishu(y):
+    y = eval(input('请输入数据:'))
+    if y <= 1:
         print('输入数据错误，请重新输入')
-    elif x == 2:
-        print('%d是质数' % x)
+    elif y == 2:
+        print('%d是质数' % y)
     else:
-        for i in (3, x + 1):
-            if x % i == 0:
-                return '%d不是质数' % x
+        for i in (3, y + 1):
+            if y % i == 0:
+                return '%d不是质数' % y
             else:
-                return '%d是质数' % x
+                return '%d是质数' % y
+
