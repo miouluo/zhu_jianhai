51efa805474b15b010a4bb75b5c21741ff96076a
zuxi_zeng <2171232322@qq.com>
2023-10-29 18:51:45 +0800

    熊猫辨认程序

---
+++ 熊猫.py
@@ -0,0 +1,10 @@
+name = input("please input his english name:")
+if name =="fuzhi":
+    b=input("how long have him stay in guangruan:")
+    if b == "3":
+        print("ok,you are a great pandas ")
+    else:
+        print("fuck off, you are not pandas")
+
+else:
+    print("sorry,i do not know you")
