3dbea6432de443e0715256236eff153366ad7c83
jiahui_Liu <13604506+ljjjh@user.noreply.gitee.com>
2023-12-10 14:07:24 +0000

    机器学习2
    Signed-off-by: jiahui_Liu <13604506+ljjjh@user.noreply.gitee.com>

---
+++ jiqixx2.py
@@ -0,0 +1,42 @@
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import pandas as pd
+
+
+def minmax_demo():
+    # 归一化
+    # 获取数据
+    data = pd.read_csv("dating.txt")
+    data = data.iloc[:, :3]
+    print("data:\n", data)
+
+    # 实例化一个转换器类
+    transfer = MinMaxScaler(feature_range=[2, 3])
+
+    # 调用fit_transform
+    data_new = transfer.fit_transform(data)
+    print("data_new:\n", data_new)
+
+    return None
+
+
+def stand_demo():
+    # 标准化
+    # 获取数据
+    data = pd.read_csv("dating.txt")
+    data = data.iloc[:, :3]
+    print("data:\n", data)
+
+    # 实例化一个转换器类
+    transfer = StandardScaler()
+
+    # 调用fit_transform
+    data_new = transfer.fit_transform(data)
+    print("data_new:\n", data_new)
+    return None
+
+
+if __name__ == "__main__":
+    # 归一化
+    # minmax_demo()
+    # 标准化
+    stand_demo()
